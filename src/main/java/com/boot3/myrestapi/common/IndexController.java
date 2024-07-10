b4ed181572adb2e11145dc9a77f4a8b39f5c0fff
mysoyul <vega2k_@naver.com>
2024-07-10 17:40:05 +0900

    IndexController update

--- src/main/java/com/boot3/myrestapi/common/IndexController.java
+++ src/main/java/com/boot3/myrestapi/common/IndexController.java
@@ -10,8 +10,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RestController
 public class IndexController {
     @GetMapping("/api")
-    public RepresentationModel index() {
-        var index = new RepresentationModel();
+    public RepresentationModel<?> index() {
+        var index = new RepresentationModel<>();
         index.add(linkTo(LectureController.class).withRel("lectures"));
         return index;
     }
