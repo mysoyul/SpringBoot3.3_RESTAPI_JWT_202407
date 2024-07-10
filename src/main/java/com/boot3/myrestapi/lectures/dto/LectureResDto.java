4efb086d994e2b31a05f9491cc4e60c63a1778c6
mysoyul <vega2k_@naver.com>
2024-07-10 15:21:32 +0900

    LectureResDto add

--- src/main/java/com/boot3/myrestapi/lectures/dto/LectureResDto.java
+++ src/main/java/com/boot3/myrestapi/lectures/dto/LectureResDto.java
@@ -1,38 +1,36 @@
-package com.boot3.myrestapi.lectures;
+package com.boot3.myrestapi.lectures.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-@Getter @Setter
-@Entity  @Table(name = "lectures")
-public class Lecture {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+@AllArgsConstructor
+public class LectureResDto {
     private Integer id;
-
-    @Column(nullable = false)
     private String name;
-
     private String description;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime beginEnrollmentDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime closeEnrollmentDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime beginLectureDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime endLectureDateTime;
-    
+
     private String location;
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
-
     private boolean offline;
     private boolean free;
-
-    @Enumerated(EnumType.STRING)
-    private LectureStatus lectureStatus = LectureStatus.DRAFT;
-}  
+    private String email;
+}
