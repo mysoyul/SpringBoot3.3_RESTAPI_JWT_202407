309de6590eaa2c71709c85c6e4f264182801c1be
mysoyul <vega2k_@naver.com>
2024-07-10 17:34:40 +0900

    LectureController self link remove

--- src/main/java/com/boot3/myrestapi/lectures/dto/LectureResource.java
+++ src/main/java/com/boot3/myrestapi/lectures/dto/LectureResource.java
@@ -1,5 +1,6 @@
 package com.boot3.myrestapi.lectures.dto;
 
+import com.boot3.myrestapi.lectures.LectureController;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
@@ -8,10 +9,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Getter
 public class LectureResource extends RepresentationModel<LectureResource> {
+    @JsonUnwrapped
     private final LectureResDto lectureResDto;
     
     public LectureResource(LectureResDto resDto) {
         this.lectureResDto = resDto;
+        //self link 추가
+        add(linkTo(LectureController.class).slash(resDto.getId()).withSelfRel());
     }
     
 
