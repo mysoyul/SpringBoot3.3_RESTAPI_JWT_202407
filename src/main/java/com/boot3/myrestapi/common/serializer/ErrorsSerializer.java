46e2309c74a2f4391385546411b8edbb8dfa3224
mysoyul <vega2k_@naver.com>
2024-07-10 16:32:33 +0900

    ErrorsSerializer update

--- src/main/java/com/boot3/myrestapi/common/serializer/ErrorsSerializer.java
+++ src/main/java/com/boot3/myrestapi/common/serializer/ErrorsSerializer.java
@@ -2,6 +2,7 @@ package com.boot3.myrestapi.common.serializer;
 
 import java.io.IOException;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.validation.Errors;
 
@@ -10,10 +11,12 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 @JsonComponent
+@Slf4j
 public class ErrorsSerializer extends JsonSerializer<Errors>{
 	@Override
 	public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
 		gen.writeStartArray();
+        //Iterable 의 forEach(Consumer) Consumer 함수형인터페이스의 추상메서드 void accept(T t);
         errors.getFieldErrors().forEach(e -> {
             try {
                 gen.writeStartObject();
@@ -27,7 +30,8 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
                 }
                 gen.writeEndObject();
             } catch (IOException e1) {
-                e1.printStackTrace();
+                //e1.printStackTrace();
+                log.error("Errors 객체 FieldError 직렬화 오류발생", e1);
             }
         });
 
@@ -39,7 +43,8 @@ public class ErrorsSerializer extends JsonSerializer<Errors>{
                 gen.writeStringField("defaultMessage", e.getDefaultMessage());
                 gen.writeEndObject();
             } catch (IOException e1) {
-                e1.printStackTrace();
+                //e1.printStackTrace();
+                log.error("Errors 객체 GlobalError 직렬화 오류발생", e1);
             }
         });
         gen.writeEndArray();
