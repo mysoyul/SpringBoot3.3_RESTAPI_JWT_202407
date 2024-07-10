1852f5ab9e4a85ca2009c4c3a1fde657eed63b4e
mysoyul <vega2k_@naver.com>
2024-07-10 17:26:19 +0900

    LectureController LecttureResource use

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,8 @@
 package com.boot3.myrestapi.lectures;
 
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
+import com.boot3.myrestapi.lectures.dto.LectureResDto;
+import com.boot3.myrestapi.lectures.dto.LectureResource;
 import com.boot3.myrestapi.lectures.validator.LectureValidator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping(value = "/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
@@ -50,12 +54,18 @@ public class LectureController {
         //free와 offline 값을 update
         lecture.update();
         Lecture addLecture = this.lectureRepository.save(lecture);
+        //Entity => ResDto 변환
+        LectureResDto lectureResDto = modelMapper.map(addLecture, LectureResDto.class);
 
-        WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder
-                .linkTo(LectureController.class)
+        WebMvcLinkBuilder selfLinkBuilder = linkTo(LectureController.class)
                 .slash(addLecture.getId());
         URI createUri = selfLinkBuilder.toUri();
-        return ResponseEntity.created(createUri).body(addLecture);
+
+        LectureResource lectureResource = new LectureResource(lectureResDto);
+        lectureResource.add(linkTo(LectureController.class).withRel("query-lectures"));
+        lectureResource.add(selfLinkBuilder.withSelfRel());
+        lectureResource.add(selfLinkBuilder.withRel("update-lecture"));
+        return ResponseEntity.created(createUri).body(lectureResource);
     }
 
     private static ResponseEntity<?> getErrors(Errors errors) {
