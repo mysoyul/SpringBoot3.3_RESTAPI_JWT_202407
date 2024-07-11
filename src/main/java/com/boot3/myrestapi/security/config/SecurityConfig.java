6554c8e8cacf03221149f04cd1ee880ce196832e
mysoyul <vega2k_@naver.com>
2024-07-11 13:31:18 +0900

    securityFilteChain bean add

--- src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
+++ src/main/java/com/boot3/myrestapi/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.boot3.myrestapi.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +21,18 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/users/welcome").permitAll()
+                            .requestMatchers("/api/lectures/**").authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
 
     @Bean
     //authentication
