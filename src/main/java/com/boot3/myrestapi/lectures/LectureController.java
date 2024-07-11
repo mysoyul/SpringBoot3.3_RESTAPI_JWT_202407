528f7837de7e10b192c4a7785109a8acbaee6bcb
mysoyul <vega2k_@naver.com>
2024-07-11 11:08:44 +0900

    BusinessException use

--- src/main/java/com/boot3/myrestapi/lectures/LectureController.java
+++ src/main/java/com/boot3/myrestapi/lectures/LectureController.java
@@ -1,6 +1,7 @@
 package com.boot3.myrestapi.lectures;
 
 import com.boot3.myrestapi.common.dto.ErrorsResource;
+import com.boot3.myrestapi.common.exception.BusinessException;
 import com.boot3.myrestapi.lectures.dto.LectureReqDto;
 import com.boot3.myrestapi.lectures.dto.LectureResDto;
 import com.boot3.myrestapi.lectures.dto.LectureResource;
@@ -16,6 +17,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -46,12 +48,14 @@ public class LectureController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getLecture(@PathVariable Integer id) {
-        Optional<Lecture> optionalLecture = this.lectureRepository.findById(id);
-        if(optionalLecture.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
+//        Optional<Lecture> optionalLecture = this.lectureRepository.findById(id);
+//        if(optionalLecture.isEmpty()) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        Lecture lecture = optionalLecture.get();
 
-        Lecture lecture = optionalLecture.get();
+        Lecture lecture = this.lectureRepository.findById(id) //Optional<Lecture>
+            .orElseThrow(() -> new BusinessException("Lecture Not Found", HttpStatus.NOT_FOUND ));
         LectureResDto lectureResDto = modelMapper.map(lecture, LectureResDto.class);
         LectureResource lectureResource = new LectureResource(lectureResDto);
         return ResponseEntity.ok(lectureResource);
