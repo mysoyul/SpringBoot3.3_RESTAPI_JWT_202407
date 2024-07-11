eef40269b40d52ff45e394476347bbe37ab4c3fc
mysoyul <vega2k_@naver.com>
2024-07-11 16:25:04 +0900

    Advice 401,403 Exception handle

--- src/main/java/com/boot3/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/boot3/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -62,5 +64,17 @@ public class DefaultExceptionAdvice {
         log.error(e.getMessage(), e);
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(500));
+    }
+
+    //401 예외 발생
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public void badCredentialExceptionHandler(BadCredentialsException e){
+        throw new BadCredentialsException(e.getMessage());
+    }
+
+    //403 예외 발생
+    @ExceptionHandler(value = AccessDeniedException.class)
+    public void accessDeniedExceptionHandler(Exception e) {
+        throw new AccessDeniedException(e.getMessage());
     }
 }
