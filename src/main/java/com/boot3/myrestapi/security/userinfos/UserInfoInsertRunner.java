a43cbccb4bf5368434713f5eac01fd9af191c897
mysoyul <vega2k_@naver.com>
2024-07-11 14:25:34 +0900

    SecurityConfig UserDetailsSvc,DaoAuthProver

--- src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoInsertRunner.java
+++ src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoInsertRunner.java
@@ -19,15 +19,13 @@ public class UserInfoInsertRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         UserInfo userInfo = new UserInfo();
         userInfo.setName("adminboot");
-        userInfo.setPassword(
-                passwordEncoder.encode("pwd1"));
+        userInfo.setPassword(passwordEncoder.encode("pwd1"));
         userInfo.setEmail("admin@aa.com");
         userInfo.setRoles("ROLE_ADMIN,ROLE_USER");
 
         UserInfo userInfo2 = new UserInfo();
         userInfo2.setName("userboot");
-        userInfo2.setPassword(
-                passwordEncoder.encode("pwd2"));
+        userInfo2.setPassword(passwordEncoder.encode("pwd2"));
         userInfo2.setEmail("user@aa.com");
         userInfo2.setRoles("ROLE_USER");
 
