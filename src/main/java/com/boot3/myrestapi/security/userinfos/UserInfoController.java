f20ffa482a4aa70fa47bf65e1810c98827466235
mysoyul <vega2k_@naver.com>
2024-07-11 15:26:20 +0900

    UserInfoController login add

--- src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoController.java
+++ src/main/java/com/boot3/myrestapi/security/userinfos/UserInfoController.java
@@ -1,6 +1,11 @@
 package com.boot3.myrestapi.security.userinfos;
 
+import com.boot3.myrestapi.security.userinfos.jwt.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +16,11 @@ public class UserInfoController {
     private UserInfoRepository repository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtService jwtService;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -24,4 +34,17 @@ public class UserInfoController {
         return savedUserInfo.getName() + " user added!!";
     }
 
+    @PostMapping("/login")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getEmail(),
+                        authRequest.getPassword()
+                ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getEmail());
+        } else {
+            throw new UsernameNotFoundException("invalid user request !");
+        }
+    }
 }
